ee146175b6ec37ec2143dd5d3f91ca56876f0630
cardumen <cardumen@MBK-Pro-MV.local>
2020-12-09 18:58:11 -0300

    menu 50%

--- js/app.js
+++ js/app.js
@@ -19,17 +19,59 @@ function toggleCloseSiderbar(){
     document.getElementById("close").classList.toggle('d-none');
 }
 
+//return home
+var returnHome = function(){
+    document.getElementById("sidebar-menu").style.display = "none";
+    document.getElementById("home-recorrido").style.display = "block";
+    scrollMenu.removeAttribute('class');
+}
+//Escuelas
+var sectionAdministracion = function(){
+    document.getElementById("sidebar-menu").style.display = "block";
+    document.getElementById("home-recorrido").style.display = "none";
+    scrollMenu.classList.toggle('administracion-active');
+}
+var sectionHumanidades = function(){
+    document.getElementById("sidebar-menu").style.display = "block";
+    document.getElementById("home-recorrido").style.display = "none";
+    scrollMenu.classList.toggle('humanidades-active');
+}
+var sectionProcesos = function(){
+    document.getElementById("sidebar-menu").style.display = "block";
+    document.getElementById("home-recorrido").style.display = "none";
+    scrollMenu.classList.toggle('procesos-active');
+}
+var sectionIngenieria = function(){
+    document.getElementById("sidebar-menu").style.display = "block";
+    document.getElementById("home-recorrido").style.display = "none";
+    scrollMenu.classList.toggle('ingenieria-active');
+}
+var sectionSalud = function(){
+    document.getElementById("sidebar-menu").style.display = "block";
+    document.getElementById("home-recorrido").style.display = "none";
+    scrollMenu.classList.toggle('salud-active');
+}
+var sectionEspacios = function(){
+    document.getElementById("sidebar-menu").style.display = "block";
+    document.getElementById("home-recorrido").style.display = "none";
+    scrollMenu.classList.toggle('espacios-comunes-active');
+}
 //Menu level 1
-function toggleSidebar(){
-    scrollMenu.classList.toggle('stgo-active');
+function escuelaAdministracion(){
+    scrollMenu.classList.toggle('administracion-active');
 }
-function toggleSidebarRancagua(){
-    scrollMenu.classList.toggle('rancagua-active');
+function escuelaHumanidades(){
+    scrollMenu.classList.toggle('humanidades-active');
 }
-function toggleSidebarLaSerena(){
-    scrollMenu.classList.toggle('laserena-active');
+function escuelaProcesos(){
+    scrollMenu.classList.toggle('procesos-active');
 }
-function toggleSidebarTemuco(){
-    scrollMenu.classList.toggle('temuco-active');
+function escuelaIngenieria(){
+    scrollMenu.classList.toggle('ingenieria-active');
 }
-
+function escuelaSalud(){
+    scrollMenu.classList.toggle('salud-active');
+}
+function escuelaEspacios(){
+    scrollMenu.classList.toggle('espacios-comunes-active');
+}
