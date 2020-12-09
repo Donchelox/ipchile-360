17cf3d4489c826e1d5bc81cd4ccee78908f36982
cardumen <cardumen@MBK-Pro-MV.local>
2020-12-09 16:21:46 -0300

    new home

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
-app.js
+//variables
+
+var scrollMenu = document.getElementById("scroll-menu");
+var aScene = document.querySelector('a-scene');
+var aSky = document.querySelector('a-sky');
+var aText = document.querySelector('a-text');
+var pausaVideo = function(){document.querySelector('video').setAttribute('src', '');}
+var titleVideo = document.getElementById("title-video");
+var startSedes = function(){
+    document.getElementById("sidebar-menu").style.display = "block";
+    aScene.style.display = "block";
+}
+
+//open menu and animation
+function toggleSiderbarMenu(){
+    document.getElementById("sidebar-menu").classList.toggle('close');
+}
+function toggleCloseSiderbar(){
+    document.getElementById("close").classList.toggle('d-none');
+}
+
+//Menu level 1
+function toggleSidebar(){
+    scrollMenu.classList.toggle('stgo-active');
+}
+function toggleSidebarRancagua(){
+    scrollMenu.classList.toggle('rancagua-active');
+}
+function toggleSidebarLaSerena(){
+    scrollMenu.classList.toggle('laserena-active');
+}
+function toggleSidebarTemuco(){
+    scrollMenu.classList.toggle('temuco-active');
+}
+
