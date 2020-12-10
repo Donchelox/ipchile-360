5aa67e72cad0cffc2ad452310c65593c6ea4acee
cardumen <cardumen@MBK-Pro-MV.local>
2020-12-09 22:07:25 -0300

    modificacion header

--- js/app.js
+++ js/app.js
@@ -24,37 +24,50 @@ var returnHome = function(){
     document.getElementById("sidebar-menu").style.display = "none";
     document.getElementById("home-recorrido").style.display = "block";
     scrollMenu.removeAttribute('class');
+    aScene.style.display = "none";
 }
 //Escuelas
 var sectionAdministracion = function(){
     document.getElementById("sidebar-menu").style.display = "block";
     document.getElementById("home-recorrido").style.display = "none";
     scrollMenu.classList.toggle('administracion-active');
+    aScene.style.display = "block";
+    aSky.setAttribute('src', 'img/360/clinica-odontologica-1.jpg');
 }
 var sectionHumanidades = function(){
     document.getElementById("sidebar-menu").style.display = "block";
     document.getElementById("home-recorrido").style.display = "none";
     scrollMenu.classList.toggle('humanidades-active');
+    aScene.style.display = "block";
+    aSky.setAttribute('src', 'img/360/clinica-odontologica-1.jpg');
 }
 var sectionProcesos = function(){
     document.getElementById("sidebar-menu").style.display = "block";
     document.getElementById("home-recorrido").style.display = "none";
     scrollMenu.classList.toggle('procesos-active');
+    aScene.style.display = "block";
+    aSky.setAttribute('src', 'img/360/clinica-odontologica-1.jpg');
 }
 var sectionIngenieria = function(){
     document.getElementById("sidebar-menu").style.display = "block";
     document.getElementById("home-recorrido").style.display = "none";
     scrollMenu.classList.toggle('ingenieria-active');
+    aScene.style.display = "block";
+    aSky.setAttribute('src', 'img/360/clinica-odontologica-1.jpg');
 }
 var sectionSalud = function(){
     document.getElementById("sidebar-menu").style.display = "block";
     document.getElementById("home-recorrido").style.display = "none";
     scrollMenu.classList.toggle('salud-active');
+    aScene.style.display = "block";
+    aSky.setAttribute('src', 'img/360/clinica-odontologica-1.jpg');
 }
 var sectionEspacios = function(){
     document.getElementById("sidebar-menu").style.display = "block";
     document.getElementById("home-recorrido").style.display = "none";
     scrollMenu.classList.toggle('espacios-comunes-active');
+    aScene.style.display = "block";
+    aSky.setAttribute('src', 'img/360/clinica-odontologica-1.jpg');
 }
 //Menu level 1
 function escuelaAdministracion(){
